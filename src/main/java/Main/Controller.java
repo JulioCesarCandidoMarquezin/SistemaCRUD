33fd06baf51cf28fa5becb689d7bbb70f7efe9f8
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-02-22 11:49:10 -0300

    commit inicial

---
+++ src/main/java/Main/Controller.java
@@ -0,0 +1,30 @@
+package Main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+    @FXML
+    BorderPane root;
+
+    @FXML
+    GridPane painelDeBotoes;
+
+    @FXML
+    Button cadastrar;
+
+    @FXML
+    Button verificar;
+
+    @FXML
+    Button ler;
+
+    @FXML
+    Button atualizar;
+
+    @FXML
+    Button deletar;
+}
