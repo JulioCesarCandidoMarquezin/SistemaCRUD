b0f13c1786bf5fc8b779a73579bf5a20cc378e79
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-03-19 00:05:48 -0400

    Alterações nos recursos do projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,24 @@ module crud.sistemacrud {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens Main to javafx.fxml;
-    exports Main;
+    opens GUI.Main to javafx.fxml;
+    exports GUI.Main;
+
+    opens GUI.Cadastro to javafx.fxml;
+    exports GUI.Cadastro;
+
+    opens GUI.Verificar to javafx.fxml;
+    exports GUI.Verificar;
+
+    opens GUI.LerInformacoes to javafx.fxml;
+    exports GUI.LerInformacoes;
+
+    opens GUI.Atualizar to javafx.fxml;
+    exports GUI.Atualizar;
+
+    opens GUI.Deletar to javafx.fxml;
+    exports GUI.Deletar;
+
+    opens GUI.GerarRelatorio to javafx.fxml;
+    exports GUI.GerarRelatorio;
 }
