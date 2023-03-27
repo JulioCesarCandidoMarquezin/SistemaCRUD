3904010665566fb0f616501d63cd0ec2ff13921b
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-03-26 23:22:49 -0400

    Criadas exceções personalizadas de acesso aos FXMLs

--- src/main/java/GUI/Exceptions/FXMLLoadException.java
+++ src/main/java/GUI/Exceptions/FXMLLoadException.java
@@ -1,6 +1,11 @@
 package GUI.Exceptions;
 
 public class FXMLLoadException extends RuntimeException{
+
+    public FXMLLoadException(){
+        super();
+    }
+
     public FXMLLoadException(String erroMensage){
         super(erroMensage);
     }
--- src/main/java/GUI/Exceptions/FXMLNotFoundException.java
+++ src/main/java/GUI/Exceptions/FXMLNotFoundException.java
@@ -1,6 +1,11 @@
 package GUI.Exceptions;
 
 public class FXMLNotFoundException extends RuntimeException{
+
+    public FXMLNotFoundException(){
+        super();
+    }
+
     public FXMLNotFoundException(String errorMensage){
         super(errorMensage);
     }
