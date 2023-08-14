fef9bbabe7605b618f8cd1e00ad334d853cca8c4
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-08-13 22:53:36 -0400

    Adicionado numeração ao enum PlanConservationStatus e método acessor o valor atual.

--- src/main/java/model/entities/enums/PlantConservationStatus.java
+++ src/main/java/model/entities/enums/PlantConservationStatus.java
@@ -1,10 +1,20 @@
 package model.entities.enums;
 
 public enum PlantConservationStatus {
-    CRITICAL,
-    ENDANGERED,
-    VULNERABLE,
-    NEAR_THREATENED,
-    LEAST_CONCERN,
-    DATA_DEFICIENT
+    CRITICAL(1),
+    ENDANGERED(2),
+    VULNERABLE(3),
+    NEAR_THREATENED(4),
+    LEAST_CONCERN(5),
+    DATA_DEFICIENT(6);
+
+    private final int value;
+
+    PlantConservationStatus(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
